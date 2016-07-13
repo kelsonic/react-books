39022ffaa908de55d42923ed7cc935be02d4fbfc
kelsonic <kgadams93@gmail.com>
2016-07-13 14:11:41 -0700

    Add pages to objects

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'JavaScript: The Good Parts' },
-    { title: 'Eloquent Ruby' },
-    { title: 'JavaScript: The Fantastic Parts' },
-    { title: 'JavaScript: The Okay Parts' }
+    { title: 'JavaScript: The Good Parts', pages: 101 },
+    { title: 'Eloquent Ruby', pages: 2 },
+    { title: 'JavaScript: The Fantastic Parts', pages: 1 },
+    { title: 'JavaScript: The Okay Parts', pages: 10 }
   ];
 }
