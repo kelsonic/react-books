4d72fc1fe3a2774e025789b0fd51d32653aafce6
kelsonic <kgadams93@gmail.com>
2016-07-13 13:05:09 -0700

    Build book list container with connect function from react-redux

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
   
   renderList() {
     return this.props.books.map((book) => {
@@ -18,3 +19,12 @@ export default class BookList extends Component {
     );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    books: state.books
+  };
+}
+
+// connect produces a container
+export default connect(mapStateToProps)(BookList);
