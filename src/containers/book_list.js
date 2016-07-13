4b7ee7739c991777f8ff0dc570896701136fb700
kelsonic <kgadams93@gmail.com>
2016-07-13 14:10:52 -0700

    Add selectBook function when item is clicked

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -1,12 +1,19 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
   
   renderList() {
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className="list-group-item">{book.title}</li>
+        <li 
+          key={book.title}
+          onClick={() => this.props.selectBook(book)}
+          className="list-group-item">
+          {book.title}
+        </li>
       );
     });
   }
@@ -26,5 +33,13 @@ function mapStateToProps(state) {
   };
 }
 
-// connect produces a container
-export default connect(mapStateToProps)(BookList);
+// Anything returned from this func will end up as props on the BookList container
+function mapDispatchToProps(dispatch) {
+  // Whenever selectBook is called, the result should be passed to all of our reducers
+  return bindActionCreators({
+    selectBook: selectBook
+  }, dispatch);
+}
+
+// Promote BookList from component to container; it needs to know about this new dispatch method, selectBook. Make it available as a prop.
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
